11f2331cb1817ae2518d6d3fb4600e721e84adce
Nasir Iqbal <mehr_nasir@hotmail.com>
2018-08-11 10:06:19 +1000

    Initial Add Project

---
+++ src/main/java/com/springframework/sfgrecipes/SfgRecipesAppApplication.java
@@ -0,0 +1,12 @@
+package com.springframework.sfgrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgRecipesAppApplication.class, args);
+	}
+}
