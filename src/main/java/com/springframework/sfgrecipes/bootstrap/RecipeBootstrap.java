30d918c551f928f50ac9fbebae809c9ba8319254
nasiriqbal786 <mehr_nasir@hotmail.com>
2018-08-17 16:36:56 +1000

    refactor

--- src/main/java/com/springframework/sfgrecipes/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/springframework/sfgrecipes/bootstrap/RecipeBootstrap.java
@@ -173,7 +173,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 
-		recipeRepository.saveAll(getRecipes());
+		recipeRepository.saveAll(this.getRecipes());
 		
 	}
 }
